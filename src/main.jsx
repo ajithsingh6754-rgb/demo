5f17f2360c410c29c300e95c707bf627915b11be
ajithsingh6754-rgb <ajithsingh6754@email.com>
2025-10-16 12:20:58 +0530

    fourth

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import todo from './todo.jsx';
 import Shared1 from './shared1.jsx';
 import Shared2 from './shared2.jsx';
 import { Todoprovider } from './context/Todocontext.jsx';
